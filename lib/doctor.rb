040408c9c0bbce4b654d9e0c73fbce9e43b58b2f
Gia Jennings <gjennings.co@gmail.com>
2020-12-13 10:37:46 -0500

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,7 +13,9 @@ class Doctor
   end
 
   def patients
-    appointments.collect {|appointment| appointment.patient}
+    @appointments.collect do |appointment|
+      appointment.patient
+    end
   end
 
   def self.all
